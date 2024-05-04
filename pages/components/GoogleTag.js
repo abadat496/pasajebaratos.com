351ee111340cd978c423df3c173ba7546d0c852e
Abadat <abadat496@gmail.com>
2024-05-04 23:52:16 +0530

    gg

--- pages/components/GoogleTag.js
+++ pages/components/GoogleTag.js
@@ -20,7 +20,7 @@ const GoogleTag = () => {
         "marginRight": 10,
         "btnPosition": "right",
         "whatsAppNumber": "+18585854484",
-        "welcomeMessage": "”★ Welcome to Travel Desk ! ★ Get set to discover amazing deals ? Explore Flights & Hotels hassle-free with us! &#9992;  Tab to start your journey! &#127757;",
+        "welcomeMessage": "”★ Welcome to Travel Desk ! ★   Get set to discover amazing deals ? Explore Flights & Hotels hassle-free with us!  Tab to start your journey!",
         "zIndex": 999999,
         "btnColorScheme": "light"
       };
