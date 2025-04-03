cd2f9a2daa04a64f32b8e0718c80635adbdce807
Abadat <abadat496@gmail.com>
2025-04-04 00:00:49 +0530

    tfn

--- constant/headerConstant.js
+++ constant/headerConstant.js
@@ -1,9 +1,7 @@
 export const logoImage = "/vouelosbaratos-logo.jpg";
 export const logoMobileImage = "logo";
 export const callIcon = "/customer-service.png";
-export const phoneNumber = "+1 855 585 2324";
+export const phoneNumber = "+1 888-338-2857";
 export const website = "pasajebaratos.com";
 export const contactEmail = "tickets@flightservicedesk.com";
 export const popupImage = "/popup-img.png";
-
-
